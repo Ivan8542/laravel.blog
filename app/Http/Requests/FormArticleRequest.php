b6de477f3e36b018973c16af0994851b475c3e32
Ivan Syrygin <ivansir112000@gmail.com>
2022-06-30 08:41:58 +0000

    add file

--- app/Http/Requests/FormArticleRequest.php
+++ app/Http/Requests/FormArticleRequest.php
@@ -14,16 +14,10 @@ class FormArticleRequest extends FormRequest
             $validateUnique = '|unique:articles';
         }
 
-        $published = '';
-        if (\request()->path() == "articles/create/page") {
-            $published = 'required';
-        }
-
-
         $attributes = request()->validate([
             'name' => 'required|min:5|max:255',
             'body' => 'required|max:255',
-            'published' => $published,
+            'published' => 'required',
             'detailed_description' => 'required',
             'character_code' => 'required|regex:/^[0-9a-zA-Z\-\_]+$/' . $validateUnique,
         ]);
